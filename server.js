f59be67d93f8f16c850073eb00bf33de8e43bd76
md-tahmid-hasan-golap <golapraj47@gmail.com>
2026-05-17 11:34:55 +0600

    add a applicationsCollection

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(
   cors({
     origin: [
       "https://e-tuition-bd-client-eight.vercel.app",
-      // "https://etutionbd-client.vercel.app", // Placeholder  
+      // "https://etutionbd-client.vercel.app", //   
     ],
     credentials: true,
   })
